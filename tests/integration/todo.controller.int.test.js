11ed9a01a185098f4608a9ffbfb616ebe268fec9
kuusmaa <4j534j@gmail.com>
2024-05-28 00:05:28 +0300

    Create error handling ofr unit and intration tests

--- tests/integration/todo.controller.int.test.js
+++ tests/integration/todo.controller.int.test.js
@@ -1,16 +1,25 @@
-const request = require("supertest")
-const app = require("../../app")
-const newTodo = require("../mock-data/new-todo.json")
+const request = require('supertest');
+const app = require('../../app');
+const newTodo = require('../mock-data/new-todo.json');
 
-const endpointUrl = "/todos/"
+const endpointUrl = '/todos/';
 
 describe(endpointUrl, () => {
-    it("POST " + endpointUrl, async () => {
+    it('POST ' + endpointUrl, async () => {
         const response = await request(app)
             .post(endpointUrl)
-            .send(newTodo)
-        expect(response.statusCode).toBe(201)
-        expect(response.body.title).toBe(newTodo.title)
-        expect(response.body.done).toBe(newTodo.done)
-    })
-}) 
+            .send(newTodo);
+        expect(response.statusCode).toBe(201);
+        expect(response.body.title).toBe(newTodo.title);
+        expect(response.body.done).toBe(newTodo.done);
+    });
+    it("should return error 500 on malformed data with POST" + endpointUrl, async () => {
+        const response = await request(app)
+            .post(endpointUrl)
+            .send({ title: "Missing done property" });
+        expect(response.statusCode).toBe(500);
+        expect(response.body).toStrictEqual({
+            message: "Todo validation failed: done: Path `done` is required."
+        });
+    });
+});
